a731d045697191756f1a3ded73c5e9eb2cac45a7
chandan-hn <121455237+chandan-hn@users.noreply.github.com>
2024-04-29 01:02:29 +0530

    Create biodata.groovy

---
+++ vars/biodata.groovy
@@ -0,0 +1,13 @@
+def call() {
+  script {
+    name="chandan"
+    age="28"
+    place"hassan"
+    job="devops"
+
+    echo "My name is: $name"
+    echo "My age is: $age"
+    echo "My place is: $place"
+    echo "My Job is: $job"
+  }
+}
